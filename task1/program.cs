e946eae8b73abec4779c0de0acc8272ab37ee8a7
Elena Kurochkina <e.kuro4kina@gmail.com>
2022-07-02 21:50:05 +0300

    task1 completed

---
+++ task1/program.cs
@@ -0,0 +1,39 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными 
+//вещественными числами
+
+int Prompt(string message)
+{
+    System.Console.WriteLine(message);
+    return int.Parse(Console.ReadLine());
+}
+
+double[,] GenerateArray(int m, int n)
+{
+    var array = new double[m, n];
+    var rnd = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rnd.NextDouble() * 100;
+              }
+    }
+    return array;
+}
+
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write($"{array[i, j]:F3}" + "\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int m = Prompt("count of ms: ");
+int n = Prompt("count of ns: ");
+double[,] array = GenerateArray(m, n);
+PrintArray(array);
